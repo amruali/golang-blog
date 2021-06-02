30ca0db2d3c44e059f5db6de144ebb1d280ccc16
amru <amrali90111@gmail.com>
2021-06-02 17:14:24 +0200

    :hammer: Updated main file.   - Connect to DB.   - Migrate database models.

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"os"
+
+	. "github.com/amrali/golang-blog/pkg/db"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	var sqlDB *sql.DB
+	var db *gorm.DB
+	{
+		var err error
+		db, err = ConnectDB()
+		if err != nil {
+			os.Exit(-1)
+		}
+		sqlDB, _ = db.DB()
+	}
+	defer sqlDB.Close()
+	Migrate(db)
+
+}
