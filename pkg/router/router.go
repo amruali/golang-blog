e5b1a90858530bc4f7513d06e2a10318d43ec11f
amru <amrali90111@gmail.com>
2021-06-03 11:10:44 +0200

    :hammer: Added new routes to HttpRequests function    - /register.    - /login.    - /logout.

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -4,10 +4,16 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	m "github.com/amrali/golang-blog/pkg/middleware"
+	. "github.com/amrali/golang-blog/pkg/handler"
 )
 
 
 func HttpRequests() {
 	r := mux.NewRouter().StrictSlash(true)
-	log.Fatal(http.ListenAndServe(":8080", r))
+	r.HandleFunc("/register", m.Middleware(Register)).Methods("POST")
+	r.HandleFunc("/login", m.Middleware(Login)).Methods("POST")
+	r.HandleFunc("/logout", m.Middleware(Logout)).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8081", r))
 }
